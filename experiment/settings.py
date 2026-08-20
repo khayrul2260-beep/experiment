8413b956f99d5abbf7a80d492957039d0b49a4f0
Khayrul Islam <khayrul2260@gmail.com>
2026-08-20 12:39:35 +0600

    Fix production settings

--- experiment/settings.py
+++ experiment/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'experiment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,7 @@ USE_TZ = True
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static/",
 ]
